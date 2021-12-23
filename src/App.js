c50edc702d088c195e5245f5aba58d3352f7f515
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2021-12-23 23:13:02 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import ErrorExample from "./tutorial/1-useState/setup/1-error-example";
+// import ErrorExample from "./tutorial/1-useState/setup/1-error-example";
+// import UseStateBasics from "./tutorial/1-useState/setup/2-useState-basics";
+import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
 
 const App = () => {
   return (
     <div className="App">
-      <ErrorExample />
+      {/* <ErrorExample /> */}
+      {/* <UseStateBasics /> */}
+      <UseStateArray />
     </div>
   );
 };
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+// import { data } from '../../../data';
 
 const UseStateArray = () => {
   return <h2>useState array example</h2>;
