68af9e224c6d54457cf04fa9dfd4d76c8483b235
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2021-12-24 16:52:05 +0100

    reminder project

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import { useState } from 'react';
+// import data from './01-birthday-reminder/setup/data';
+import List from "./01-birthday-reminder/setup/List";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <h2>reminder project setup</h2>
+      <List />
+    </>
   );
 }
 
