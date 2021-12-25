d79f6184a8da61b70be6c9dd2367b54dbfc584ae
Loveday-Alfred <alfredemmanuel2018@yahoo.com>
2021-12-25 23:03:43 +0100

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+// useState Basics
 // import ErrorExample from "./tutorial/1-useState/setup/1-error-example";
 // import UseStateBasics from "./tutorial/1-useState/setup/2-useState-basics";
 // import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
 // import UseStateObject from "./tutorial/1-useState/setup/4-useState-object";
-import UseStateCounter from "./tutorial/1-useState/setup/5-useState-counter";
+// import UseStateCounter from "./tutorial/1-useState/setup/5-useState-counter";
+
+// useEffect Basics
+import UseEffectBasics from "./tutorial/2-useEffect/setup/1-useEffect-basics";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="container">
+      {/* useState basics  */}
       {/* <ErrorExample /> */}
       {/* <UseStateBasics /> */}
       {/* <UseStateArray /> */}
       {/* <UseStateObject /> */}
-      <UseStateCounter />
+      {/* <UseStateCounter /> */}
+
+      {/* useEffectBasics */}
+      <UseEffectBasics />
     </div>
   );
 };
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
